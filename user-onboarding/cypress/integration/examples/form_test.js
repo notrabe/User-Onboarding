b71b6b7502fd1a14ce94b2e625e7e6ea3cf7b6c4
Evan Barton <notrabe1011@gmail.com>
2020-08-21 08:45:15 -0400

    finished tests

--- user-onboarding/cypress/integration/examples/form_test.js
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -1,6 +1,6 @@
 describe('user-onboarding', () => {
     it('can navigate to the page', () => {
-        cy.visit('http://localhost:3002')
+        cy.visit('http://localhost:3000')
         cy.url().should('include', 'localhost')
     })
 
@@ -8,5 +8,30 @@ describe('user-onboarding', () => {
         cy.get('input[name="name"]')
           .type('evan')
           .should('have.value', 'evan')
+    })
+    it('can type in the email input', () => {
+        cy.get('input[name="email"]')
+        .type('evan@evan.com')
+        .should('have.value', 'evan@evan.com')
+    })
+    it('can type in the password input', () => {
+        cy.get('input[name="password"]')
+        .type('evanevan')
+        .should('have.value', 'evanevan')
+    })
+    it('can check the TOS box', () => {
+        cy.get('input[name="tos"]')
+        .check()
+    })
+    it('can submit form data', () => {
+        cy.get('form')
+        .submit()
+
+    })
+    it('can validate the form', () => {
+        cy.get('input[name="name"]')
+        .type('x')
+        cy.get('input[name="email"]')
+        .type('x.com')
     })
 })
